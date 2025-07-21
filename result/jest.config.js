4b5a024b5624ce9689f227fe5ee1a5115816952c
= <=>
2025-07-21 02:02:13 -0300

    sacar jest

--- result/jest.config.js
+++ result/jest.config.js
@@ -2,11 +2,5 @@ const { reporters } = require("mocha");
 
 module.exports = {
     testEnvironment: 'node',
-    reporters: [
-        'default',
-        ['jest-junit', {
-            outputDirectory: '.',
-            outputName: 'jest-junit.xml',
-        }]
-    ]
+    
 };
