d76d5e686a80a00ac42fccbc775cf84bb4f50ae7
= <=>
2025-07-22 11:27:41 -0300

    act

--- vote/app.py
+++ vote/app.py
@@ -16,11 +16,7 @@ gunicorn_error_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers.extend(gunicorn_error_logger.handlers)
 app.logger.setLevel(logging.INFO)
 
-#def get_redis():
-#    if not hasattr(g, 'redis'):
-#        redis_host = os.getenv('REDIS_HOST', 'localhost')
-#        g.redis = Redis(host=redis_host, db=0, socket_timeout=5)
-#    return g.redis
+
 
 def get_redis():
     if not hasattr(g, 'redis'):
